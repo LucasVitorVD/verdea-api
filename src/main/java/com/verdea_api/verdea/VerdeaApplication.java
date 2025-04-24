d071246d31f272650d5c010e9eb26a357cad1cc8
LucasVitorVD <lucaskirito85@gmail.com>
2025-04-24 16:26:50 -0300

    base project

---
+++ src/main/java/com/verdea_api/verdea/VerdeaApplication.java
@@ -0,0 +1,13 @@
+package com.verdea_api.verdea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verdea_api/verdea/VerdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verdea_api.verdea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
